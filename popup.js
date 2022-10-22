ee8b6b8c60e8319dc6e6669e5b75d46e0f9745d6
José María <josemmazzei@gmail.com>
2022-10-22 18:41:53 -0300

    Add destination form without functionality

--- popup.js
+++ popup.js
@@ -1,9 +1,8 @@
 import { getActiveTabId } from './utils.js'
 
-const btn = document.getElementById('btn')
 const div = document.getElementsByTagName('div')[0]
-
-async function btnEvent() {
+const btn = document.getElementById('btn')
+async function addOption() {
   const activeTabId = await getActiveTabId()
   chrome.tabs.sendMessage(
     activeTabId,
@@ -17,5 +16,35 @@ async function btnEvent() {
     },
   )
 }
+btn.addEventListener('click', addOption)
 
-btn.addEventListener('click', btnEvent)
+const destinationForm = document.getElementById('destination')
+function addDestination(e) {
+  e.preventDefault()
+  const p = document.createElement('p')
+  p.textContent = 'destination added'
+  div.append(p)
+}
+destinationForm.addEventListener('submit', addDestination)
+
+const showDestinationContainer = document.getElementById(
+  'showDestinationContainer',
+)
+function showAddDestinationForm() {
+  const destinationContainer = document.getElementsByClassName(
+    'destinationContainer',
+  )[0]
+  destinationContainer.classList.add('active')
+}
+showDestinationContainer.addEventListener('click', showAddDestinationForm)
+
+const hideDestinationContainer = document.getElementById(
+  'hideDestinationContainer',
+)
+function closeAddDestinationForm() {
+  const destinationContainer = document.getElementsByClassName(
+    'destinationContainer',
+  )[0]
+  destinationContainer.classList.remove('active')
+}
+hideDestinationContainer.addEventListener('click', closeAddDestinationForm)
